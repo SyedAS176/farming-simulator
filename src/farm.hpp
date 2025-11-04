caf7a477da51811e31e397893f98fe779a2d2732
Redacted <146182187+SyedAS176@users.noreply.github.com>
2025-11-04 08:18:42 -0600

    Added day variable and get_day() method.

--- src/farm.hpp
+++ src/farm.hpp
@@ -10,6 +10,7 @@ class Farm {
 private:
     int rows;
     int columns;
+    int day = 1;            // MOVE THIS WHEN YOU MAKE A WORLD LOGIC CLASS; which i shouldve done in the beginning but i got lazy
     Player *player;
     std::vector<std::vector<Plot *>> plots;
 
@@ -20,6 +21,7 @@ public:
     // Farm getter methods
     int number_of_rows();
     int number_of_columns();
+    int get_day();          // MOVE THIS WHEN YOU MAKE A WORLD LOGIC CLASS
     std::string get_symbol(int row, int column);
 
     // Farm functionality logic methods
