bf967f35d23dbdc11b354309bbe3d688374f0b19
Redacted <146182187+SyedAS176@users.noreply.github.com>
2025-11-04 08:18:04 -0600

    Added a day counter header.

--- src/farm_printer.cpp
+++ src/farm_printer.cpp
@@ -3,9 +3,13 @@
 #include "farm.hpp"
 #include "farm_printer.hpp"
 
+#include <iostream>
+#include <ostream>
+
 FarmPrinter::FarmPrinter(Farm *farm) : farm(farm) {}
 
 std::string FarmPrinter::pp() {
+    std::cout << "======= DAY " << farm->get_day() << " =======" << std::endl;
     std::string output = "";
     for(int i = 0; i < farm->number_of_rows(); i++) {
         for(int j = 0; j < farm->number_of_columns(); j++) {
