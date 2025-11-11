b330f047f60c6ea40cdd7421247bc62af1879757
Redacted <146182187+SyedAS176@users.noreply.github.com>
2025-11-11 14:57:01 -0600

    Reverted back to the original code. I have a better way to implement this.

--- src/carrot.hpp
+++ src/carrot.hpp
@@ -8,12 +8,9 @@ class Carrot : public Plot {
 private:
     int age = 0;
     int mature_age = 1;
-    bool is_watered = false;
 
 public:
     std::string symbol() override;
     void end_day() override;
     bool is_mature() override;
-    int get_age();
-    void water();
 };
