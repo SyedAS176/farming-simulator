c2dbc7458042443a5758a5b7e9d4227ff1ca5e11
Redacted <146182187+SyedAS176@users.noreply.github.com>
2025-11-24 00:10:15 -0600

    Updated the legend with new plants and controls information.

--- src/legend.cpp
+++ src/legend.cpp
@@ -4,7 +4,13 @@ void print_legend() {
     std::cout << "======= FARMING SIMULATOR LEGEND =======" << std::endl;
     std::cout << "@ : Your player" << std::endl;
     std::cout << ". : Empty soil plot" << std::endl;
-    std::cout << "v / V : Carrot (or other crops)" << std::endl;
+    std::cout << "# : Tilled soil plot" << std::endl;
+    std::cout << "c / C : Carrot" << std::endl;
+    std::cout << "l / L : Lettuce" << std::endl;
+    std::cout << "s / S : Spinach" << std::endl;
+    std::cout << "b / B : Beet" << std::endl;
+    std::cout << "bs / BS : Brussels" << std::endl;
+
     std::cout << std::endl;
 
     std::cout << "CONTROLS:" << std::endl;
@@ -12,10 +18,16 @@ void print_legend() {
     std::cout << "A : Move Left" << std::endl;
     std::cout << "S : Move Down" << std::endl;
     std::cout << "D : Move Right" << std::endl;
-    std::cout << "P : Plant a vegetable at your current location" << std::endl;
+
+    std::cout << "c/C : Plant a carrot at your current location" << std::endl;
+    std::cout << "l/L : Plant a lettuce at your current location" << std::endl;
+    std::cout << "s/S : Plant a spinach at your current location" << std::endl;
+    std::cout << "b/B : Plant a beet at your current location" << std::endl;
+    std::cout << "bs/BS : Plant a brussels sprouts at your current location" << std::endl;
+
     std::cout << "H : Harvest a vegetable at your current location" << std::endl;
     std::cout << "E : End the day" << std::endl;
-    std::cout << "L : Print out the game legend" << std::endl;
+    std::cout << "? : Print out the game legend" << std::endl;
     std::cout << "Q : Quit the game" << std::endl;
     std::cout << "=================================" << std::endl;
 }
