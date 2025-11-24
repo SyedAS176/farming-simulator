f3b6d3f5fecdef6cd837c8acf4f0607876989ae7
Redacted <146182187+SyedAS176@users.noreply.github.com>
2025-11-24 00:20:54 -0600

    Updated lettuce testing logic.

--- tests/lettuce_test.cpp
+++ tests/lettuce_test.cpp
@@ -5,16 +5,92 @@
 
 #include "../src/lettuce.hpp"
 
-TEST_CASE( "it returns a v as its when it is a seedling" ) {
+TEST_CASE( "lettuce returns # as its symbol when it is tilled soil" ) {
     Lettuce lettuce;
-    REQUIRE( lettuce.symbol() == "v" );
-    REQUIRE(!lettuce.is_mature() == true); // It is true that the carrot is not mature
-
+    REQUIRE( lettuce.symbol() == "#" );
 }
 
-TEST_CASE( "Carrot matures after one day" ) {
+TEST_CASE( "lettuce returns # after one day (not yet sprouted)" ) {
     Lettuce lettuce;
-    lettuce.end_day(); // Age becomes 1
-    REQUIRE(lettuce.symbol() == "V");
-    REQUIRE(lettuce.is_mature() == true); // It is true that the carrot is mature now
+    lettuce.end_day();
+    REQUIRE( lettuce.symbol() == "#" );
+}
+
+TEST_CASE( "lettuce returns l when it is a seedling (day 2)" ) {
+    Lettuce lettuce;
+    lettuce.end_day();
+    lettuce.end_day();
+    REQUIRE( lettuce.symbol() == "l" );
+}
+
+TEST_CASE( "lettuce returns l on day 3" ) {
+    Lettuce lettuce;
+    lettuce.end_day();
+    lettuce.end_day();
+    lettuce.end_day();
+    REQUIRE( lettuce.symbol() == "l" );
+}
+
+TEST_CASE( "lettuce returns L when fully mature (day 4)" ) {
+    Lettuce lettuce;
+    lettuce.end_day();
+    lettuce.end_day();
+    lettuce.end_day();
+    lettuce.end_day();
+    REQUIRE( lettuce.symbol() == "L" );
+}
+
+TEST_CASE( "lettuce is not mature on day zero" ) {
+    Lettuce lettuce;
+    REQUIRE( lettuce.is_mature() == false );
+}
+
+TEST_CASE( "lettuce is not mature after 3 days" ) {
+    Lettuce lettuce;
+    lettuce.end_day();
+    lettuce.end_day();
+    lettuce.end_day();
+    REQUIRE( lettuce.is_mature() == false );
+}
+
+TEST_CASE( "lettuce is mature after 4 days" ) {
+    Lettuce lettuce;
+    lettuce.end_day();
+    lettuce.end_day();
+    lettuce.end_day();
+    lettuce.end_day();
+    REQUIRE( lettuce.is_mature() == true );
+}
+
+TEST_CASE( "lettuce increases age by 2 when watered" ) {
+    Lettuce lettuce;
+    lettuce.water();
+    lettuce.end_day();
+    REQUIRE( lettuce.get_age() == 2 );
+}
+
+TEST_CASE( "watering lettuce more than once per day has no effect" ) {
+    Lettuce lettuce;
+    lettuce.water();
+    lettuce.water();
+    lettuce.end_day();
+    REQUIRE( lettuce.get_age() == 2 );
+}
+
+TEST_CASE( "watering lettuce should not affect future days" ) {
+    Lettuce lettuce;
+    lettuce.water();
+    lettuce.end_day();
+    lettuce.end_day();
+    REQUIRE( lettuce.get_age() == 3 );
+}
+
+TEST_CASE( "lettuce can be matured early with watering" ) {
+    Lettuce lettuce;
+    lettuce.water();
+    lettuce.end_day();
+    lettuce.water();
+    lettuce.end_day();
+    REQUIRE( lettuce.is_mature() == true );
+    REQUIRE( lettuce.get_age() == 4 );
 }
